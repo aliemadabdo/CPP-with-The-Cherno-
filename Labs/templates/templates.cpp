6ecece11027e1d64dcc6ff17c216bead6b75be78
Ali Emad <90727865+aliemadabdo@users.noreply.github.com>
2024-09-10 03:05:11 +0300

    template specialization

--- Labs/templates/templates.cpp
+++ Labs/templates/templates.cpp
@@ -1,11 +1,26 @@
 #include <iostream>
 
+struct Complex{
+    float real;
+    float imaginary;
+
+    Complex(float x, float y) : real(x), imaginary(y) {}
+};
+
 /* FUNCTION TEMPLATE */
 template<typename T>
 void print(T value){
     std::cout << value << std::endl;
     std::cout << sizeof(T) << std::endl;
 }
+
+/* FUNCTION TEMPLATE Specialization  */
+template<>
+void print<Complex>(Complex value){
+    std::cout << value.real << "+i" << value.imaginary << std::endl;
+    std::cout << sizeof(Complex) << std::endl;
+}
+
 
 template<class T> // not created ever if it isn't called
 void printButWithClassTemp(T value){
@@ -14,8 +29,8 @@ void printButWithClassTemp(T value){
 
 }
 
-/* CLASS TEMPLATE */
-template<typename T, int N>
+
+/* CLASS TEMPLATE Specialization */template<typename T, int N>
 class Array{
 private:
     T arr[N];
@@ -25,11 +40,28 @@ public:
     }
 };
 
+template<>
+class Array<bool, 2>{
+private:
+    bool arr[2] = {true, false};
+public:
+    int getSize() const {
+        std::cout << arr[0] << " | " << arr[1] << std::endl;
+        return 2;
+    }
+};
+
+
 int main(){
-  
+    
     Array<int, 67> arr; // nore the syntax of the class declration
+    Array<bool, 2> spetialArr; // 
+ 
     print(arr.getSize());
+    print(spetialArr.getSize());
     print("ali");
     print<char>('a');
     printButWithClassTemp(2.4);
+
+    print(Complex(3, 4));
 }
