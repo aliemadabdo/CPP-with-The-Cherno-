54d65a5357b4b93feb6ed9a4b1c7f7a38b3184d3
Ali Emad <90727865+aliemadabdo@users.noreply.github.com>
2024-09-16 08:42:26 +0300

    PS46: solved (with chatgpt hints for constructing the graph)

--- Problem_sloving/12_Graphs_DFS/46. Evaluate Division/mine.cpp
+++ Problem_sloving/12_Graphs_DFS/46. Evaluate Division/mine.cpp
@@ -1,11 +1,45 @@
+/* Stopped solving this problem due to:
+ high time and memory complexity i used 
+ and the need for exploring more about the adjecancy list graph
+ in addation for taking too much time (2 days)
+ 
+ what i have leant:
+ - creating graph
+ - dealing with string-based graphs
+ - dfs with graphs
+ */
+
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <unordered_set>
 
 using namespace std;
 
 class Solution {
 public:
+
+    double dfs(const string& start, const string& end, unordered_map<string, vector<pair<string, double>>>& graph, unordered_set<string> isVisited){
+        if (graph.find(start) == graph.end()) return -1.0;
+        if (start == end) return 1.0;
+        
+        isVisited.insert(start);
+        
+        for (const auto& neighbor : graph[start]) {
+            const string& next = neighbor.first;
+            double value = neighbor.second;
+            
+            if (isVisited.find(next) == isVisited.end()) {
+                double result = dfs(next, end, graph, isVisited);
+                if (result != -1.0) {
+                    return result * value;
+                }
+            }
+        }
+        
+        return -1.0;
+    }
+
     vector<double> calcEquation(vector<vector<string>>& equations, vector<double>& values, vector<vector<string>>& queries) {
         // construct the graph adjacency matrix
         // search for queries elements in the matrix 
@@ -16,34 +50,68 @@ public:
         // how to search in the matrix efficintly
         // how to calculate the result of the query if found
 
-        // after the first row is filled we can allocate the rest of the columns
+        // // after the first row is filled we can allocate the rest of the columns
         
-        //But first we need a map the string values to matrix indices
-        unordered_map <string, int> locations;
+        // //But first we need a map the string values to matrix indices
+        // unordered_map <string, int> locations;
+        // int numOfEqns = equations.size();
+        
+        // for(int i=0; i<numOfEqns; i++){
+        //     for(int j=0; j<2; j++)
+        //         locations[equations[i][j]] = i*numOfEqns + j;    
+        // }
 
-        for(int i=0; i<equations.size(); i++){
-            // if not found i++
-            locations[equations[i][0]] = i;
-            // if not found i++
-            locations[equations[i][1]] = i;    
+        // // or we shall just use 2*equations.size() square matrix 
+        
+        // // This will be weighted directed graph 
+        // vector<vector<int>> connections(2*numOfEqns, vector<int>(2*numOfEqns, 0));
+        
+        // for (int i=0; i<2*numOfEqns; i++ ){
+        //     for (int j=0; j<numOfEqns; j++ ){
+        //         for(int k=0; k<2; k++){
+                    
+        //             connections[locations[equations[i][k]]][locations[equations[j][k]]] = values[j]; 
+
+        //             connections[locations[equations[i][k]]][locations[equations[j][0]]] = 1/values[j];
+
+        //         }
+        //     }
+        // }
+
+        // Graph to store the equations
+        unordered_map<string, vector<pair<string, double>>> graph; // you have to think abou the pair from the first 
+                                                                   // any reused letters are now unique by its weight     
+        // Build the graph
+        for (int i = 0; i < equations.size(); ++i) {
+            string A = equations[i][0];
+            string B = equations[i][1];
+            double value = values[i];
+            graph[A].emplace_back(B, value);
+            graph[B].emplace_back(A, 1.0 / value);
         }
 
-        int numOfstrings = locations.size();
-        // This will be weighted directed graph 
-        vector<vector<int>> connections(numOfstrings, vector<int>(numOfstrings, 0));
-        
-        for (int i=0; i<numOfstrings; i++ ){
-            for (int j=0; j<numOfstrings; j++ ){
-                
-                connections[locations[equations[i][0]]][locations[equations[i][1]]] = values[i];
-                connections[locations[equations[i][1]]][locations[equations[i][0]]] = 1/values[i];
-
+        vector<double> result;
+        for (const auto& query : queries) {
+            string C = query[0];
+            string D = query[1];
+            
+            // If either variable doesn't exist in the graph
+            if (graph.find(C) == graph.end() || graph.find(D) == graph.end()) {
+                result.push_back(-1.0);
+            } else {
+                unordered_set<string> visited;
+                double res = dfs(C, D, graph, visited);
+                result.push_back(res);
             }
         }
+
+        return result;
 
 
     }
 };
+
+// 
 
 // Main function to test the calcEquation function
 int main() {
---
+++ Problem_sloving/13_Graphs_BFS/47. Nearest Exit from Entrance in Maze/mine.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        
+    }
+};
